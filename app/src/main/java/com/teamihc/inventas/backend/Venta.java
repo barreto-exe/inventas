02fdc38950543078538f552fe7b7514526f37649
Karen Morán <kamoran.19@est.ucab.edu.ve>
2020-12-04 23:04:42 -0400

    Completando clase Venta

--- app/src/main/java/com/teamihc/inventas/backend/Venta.java
+++ app/src/main/java/com/teamihc/inventas/backend/Venta.java
@@ -5,5 +5,43 @@ package com.teamihc.inventas.backend;
  */
 public class Venta
 {
-    
+    //<editor-fold defaultstate="collapsed" desc="Atributos">
+    /**
+     * ID correspondiente a la venta.
+     */
+    private int idVenta;
+
+
+    /**
+     * ID tasa del día.
+     */
+    private int idRate;
+
+
+    /**
+     * Monto total a pagar.
+     */
+    private float total;
+
+
+    /**
+     * Fecha de la venta.
+     */
+    private String date;
+
+
+    /**
+     * Hora de la venta.
+     */
+    private String time;
+
+    //</editor-fold>
+
+
+    public Venta(int idRate, String date, String time)
+    {
+        this.idRate = idRate;
+        this.date = date;
+        this.time = time;
+    }
 }
