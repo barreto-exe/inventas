e4b7c115d624afc6d93227f0b55ba9c054dc776d
Karen Morán <kamoran.19@est.ucab.edu.ve>
2020-12-06 06:53:11 -0400

    Clase Carrito
    (me falta agregarle unas cositas a Ventas y queda listo)

---
+++ app/src/main/java/com/teamihc/inventas/backend/entidades/ArticuloPxQ.java
@@ -0,0 +1,40 @@
+package com.teamihc.inventas.backend.entidades;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+
+/**
+ * Clase que relaciona un artículo con la cantidad de unidades vendidas y el subtotal a pagar
+ * por las mismas. Las instancias de esta clase conforman el carrito de una venta.
+ * @author Karen
+ */
+public class ArticuloPxQ
+{
+    //<editor-fold defaultstate="collapsed" desc="Atributos">
+    private Articulo articulo;
+    private int cantidad;
+    private float subTotal;
+    //</editor-fold>
+
+    /**
+     * @param articulo es el artículo que se desea vender.
+     * @param cantidad es la cantidad de unidades que se desea vender de dicho artículo.
+     */
+    public ArticuloPxQ(@NotNull Articulo articulo, int cantidad)
+    {
+        this.articulo = articulo;
+        this.cantidad = cantidad;
+        subTotal = articulo.getPrecio() * cantidad;
+    }
+
+    //<editor-fold desc="Getters & Setters">
+    public Articulo getArticulo() { return articulo; }
+    public void setArticulo(@NotNull Articulo articulo) { this.articulo = articulo; subTotal = articulo.getPrecio() * cantidad;}
+    public int getCantidad() { return cantidad; }
+    public void setCantidad(int cantidad) { this.cantidad = cantidad; subTotal = articulo.getPrecio() * cantidad;}
+    public float getSubTotal() { return subTotal; }
+    //</editor-fold>
+
+
+}
