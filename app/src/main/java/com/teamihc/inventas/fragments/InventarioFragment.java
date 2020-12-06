027282f5e68816c4949af5c6f062964b1999f13b
Luis Barreto <luis.barreto.marin@gmail.com>
2020-12-05 22:00:57 -0400

    Ordenando constructor de Artículo

--- app/src/main/java/com/teamihc/inventas/fragments/InventarioFragment.java
+++ app/src/main/java/com/teamihc/inventas/fragments/InventarioFragment.java
@@ -26,47 +26,48 @@ public class InventarioFragment extends Fragment
     ListaProductosRecyclerViewAdapter.ListaProductosAdapter listaProductosAdapter;
     RecyclerView recyclerView;
     private ArrayList<Articulo> listaArticulos;
+    
     //ArrayList de los productos
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState)
     {
-        View view=inflater.inflate(R.layout.fragment_inventario, container, false);
+        View view = inflater.inflate(R.layout.fragment_inventario, container, false);
         recyclerView = (RecyclerView) view.findViewById(R.id.productos_inventarioRV);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false));
-
+        
         listaArticulos = new ArrayList<Articulo>();
-
+        
         cargarLista();
         mostrarDatos();
-
+        
         return view;
         
     }
-
+    
     //como actualizar la lista??
     //Como validar???
-    public void cargarLista(){
+    public void cargarLista()
+    {
         listaArticulos.clear();
         String query = "SELECT * FROM v_articulos ORDER BY descripcion ASC";
         DBOperacion op = new DBOperacion(query);
         DBMatriz resultado = op.consultar();
-
-        while(resultado.leer()){
+        
+        while (resultado.leer())
+        {
             Articulo articulo = new Articulo(
-                    (String)resultado.getValor("descripcion"),
+                    (String) resultado.getValor("descripcion"),
                     (Float) resultado.getValor("costo_unitario"),
-                    (Float)resultado.getValor("precio_venta"),
-                    //(String)resultado.getValor("codigo"),
-                    "0000",
-                    (Integer)resultado.getValor("cantidad"),
-                    null);
+                    (Float) resultado.getValor("precio_venta"),
+                    (Integer) resultado.getValor("cantidad"),
+                    "0000");
             listaArticulos.add(articulo);
         }
-
     }
-
-    public void mostrarDatos(){
+    
+    public void mostrarDatos()
+    {
         //recyclerView.clearFocus();
         InventarioAdapter adapter = new InventarioAdapter(listaArticulos);
         recyclerView.setAdapter(adapter);
