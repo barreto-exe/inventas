d2676a1ea580299cde6428777d859d86e3b8e4b6
Gustavo Gutiérrez <gustavo.dgs19@gmail.com>
2020-12-07 13:06:10 -0400

    intento de menu 1

--- app/src/main/java/com/teamihc/inventas/activities/CarritoActivity.java
+++ app/src/main/java/com/teamihc/inventas/activities/CarritoActivity.java
@@ -1,10 +1,13 @@
 package com.teamihc.inventas.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.teamihc.inventas.R;
 
@@ -17,12 +20,32 @@ public class CarritoActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_carrito);
-        
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_carrito);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.carrito_aceptar:
+                Toast.makeText(this, "aceptar", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.carrito_cancelar:
+                Toast.makeText(this, "cancelar", Toast.LENGTH_SHORT).show();
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
     
     public void add_carrito(View view)
     {
-        Intent intent = new Intent(this, AgregarArticuloActivity.class);
-        startActivity(intent);
+        //Intent intent = new Intent(this, AgregarArticuloActivity.class);
+        //startActivity(intent);
     }
 }
