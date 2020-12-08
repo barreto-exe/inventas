70c724db884cfc56943990a29e30cacc1cddfc15
Luisa <lmlopez.19@est.ucab.edu.ve>
2020-12-08 01:15:46 -0400

    Adelanto de estadísticas

--- app/src/main/java/com/teamihc/inventas/fragments/TasasFragment.java
+++ app/src/main/java/com/teamihc/inventas/fragments/TasasFragment.java
@@ -2,6 +2,7 @@ package com.teamihc.inventas.fragments;
 
 import android.app.Fragment;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,8 +12,14 @@ import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
 import com.teamihc.inventas.R;
 import com.teamihc.inventas.backend.entidades.Tasa;
+
+import java.util.ArrayList;
 
 
 public class TasasFragment extends Fragment
@@ -21,7 +28,7 @@ public class TasasFragment extends Fragment
     private TextView tasaDia;
     private TextView tasaAnterior;
     private ImageView iconoSubida;
-    
+    ArrayList<BarEntry> cambioTasa;
     //aqui se vacia la info de la tasa actual y de la estadistica del cambio de tasas
     @Nullable
     @Override
@@ -29,6 +36,23 @@ public class TasasFragment extends Fragment
     {
         view = inflater.inflate(R.layout.fragment_tasas, container, false);
         refrescarTasaDia();
+        BarChart barChart = view.findViewById(R.id.tasaChart);
+        llenarChart();
+        BarDataSet barDataSet = new BarDataSet(cambioTasa, "Cambio de la tasa del dólar");
+        barDataSet.setColor(Color.BLUE);
+        barDataSet.setValueTextColor(Color.BLACK);
+        barDataSet.setValueTextSize(16f);
+
+        BarData barData=new BarData(barDataSet);
+        barChart.setFitBars(true);
+        barData.setBarWidth(2f);
+
+        barChart.setData(barData);
+        barChart.getDescription().setText("Tasa");
+        barChart.animateY(2000);
+
+
+
         return view;
     }
     
@@ -38,7 +62,17 @@ public class TasasFragment extends Fragment
         super.onResume();
         refrescarTasaDia();
     }
-    
+
+    public void llenarChart(){
+        cambioTasa = new ArrayList<>();
+        cambioTasa.add(new BarEntry(20,192));
+        cambioTasa.add(new BarEntry(30,532));
+        cambioTasa.add(new BarEntry(60,130));
+        cambioTasa.add(new BarEntry(70,323));
+        cambioTasa.add(new BarEntry(88,224));
+        cambioTasa.add(new BarEntry(90,192));
+    }
+
     /**
      * Muestra la tasa del día y la anterior en la vista.
      */
