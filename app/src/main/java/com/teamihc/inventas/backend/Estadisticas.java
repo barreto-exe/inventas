483a350ea762abd6cff8481318cc5e81fde09a77
Karen Morán <kamoran.19@est.ucab.edu.ve>
2020-12-08 22:03:15 -0400

    Más funciones

--- app/src/main/java/com/teamihc/inventas/backend/Estadisticas.java
+++ app/src/main/java/com/teamihc/inventas/backend/Estadisticas.java
@@ -9,6 +9,10 @@ import java.util.Calendar;
  */
 public class Estadisticas
 {
+    /**
+     *
+     * @return retorna arreglo de strings con fecha de cada dia de la semana en curso.
+     */
     private static String[] diasSemana()
     {
         String dias[] = new String[7];
@@ -38,8 +42,33 @@ public class Estadisticas
         return dias;
     }
 
+    private static String intToDay(int index)
+    {
+        switch (index)
+        {
+            case 0:
+                return "Lunes";
+            case 1:
+                return "Martes";
+            case 2:
+                return "Miercoles";
+            case 3:
+                return "Jueves";
+            case 4:
+                return "Viernes";
+            case 5:
+                return "Sábado";
+            case 6:
+                return "Domingo";
+        }
+        return null;
+    }
 
-    public static float gananciaSemanal()
+    /**
+     * Calcula la ganancia total en la semana en curso.
+     * @return monto de la ganancia obtenida en la semana.
+     */
+    public static float gananciaTotalSemanal()
     {
         float ganancia = 0;
         String dia[] = diasSemana();
@@ -52,7 +81,50 @@ public class Estadisticas
         return ganancia;
     }
 
-    public static float ingresoSemanal()
+    /**
+     * Guarda en un arreglo la ganancia obtenida cada día.
+     * [0] Lunes.
+     * [1] Martes.
+     * [2] Miercoles.
+     * [3] Jueves.
+     * [4] Viernes.
+     * [5] Sábado.
+     * [6] Domingo.
+     * @param gananciaDiaria es el arreglo donde se gruardarán los datos. NOTA: el tamaño del arreglo debe ser siete (7).
+     */
+    public static void calcularGananciaDiaria(float[] gananciaDiaria)
+    {
+        String dia[] = diasSemana();
+
+        for (int i = 0; i < 7; i++)
+            gananciaDiaria[i] = Venta.obtenerGananciaDia(dia[i]);
+    }
+
+    /**
+     * Guarda en un arreglo la ganancia obtenida cada día.
+     * Posiciones:
+     * [0] Lunes.
+     * [1] Martes.
+     * [2] Miercoles.
+     * [3] Jueves.
+     * [4] Viernes.
+     * [5] Sábado.
+     * [6] Domingo.
+     * @param ingresoDiario es el arreglo donde se gruardarán los datos. NOTA: el tamaño del arreglo debe ser siete (7).
+     */
+    public static void calcularIngresoDiario(float[] ingresoDiario)
+    {
+        String dia[] = diasSemana();
+
+        for (int i = 0; i < 7; i++)
+            ingresoDiario[i] = Venta.obtenerIngresoDia(dia[i]);
+    }
+
+    /**
+     * Calcula el ingreso total en la semana en curso.
+     * @return monto del ingreso obtenid en la semana.
+     */
+    public static float ingresoTotalSemanal()
     {
         float ingreso = 0;
         String dia[] = diasSemana();
@@ -65,5 +137,46 @@ public class Estadisticas
         return ingreso;
     }
 
-    
+    /**
+     * @return string con el nombre del día con mayor ganancia.
+     */
+    public static String diaMayorGanancia()
+    {
+        String diaSemana[] = diasSemana();
+        float gananciasDiarias[] = new float[7];
+        calcularGananciaDiaria(gananciasDiarias);
+        float gananciaMayor = gananciasDiarias[0];
+        int indexDia = -1;
+
+        for (int i = 0; i < 7; i++)
+            if (gananciasDiarias[i] > gananciaMayor)
+            {
+                indexDia = i;
+                gananciaMayor = gananciasDiarias[i];
+            }
+
+        return intToDay(indexDia);
+    }
+
+    /**
+     * @return string con el nombre del día con mayor ingreso.
+     */
+    public static String diaMayorIngreso()
+    {
+        String diaSemana[] = diasSemana();
+        float ingresosDiarios[] = new float[7];
+        calcularIngresoDiario(ingresosDiarios);
+        float ingresoMayor = ingresosDiarios[0];
+        int indexDia = -1;
+
+        for (int i = 0; i < 7; i++)
+            if (ingresosDiarios[i] > ingresoMayor)
+            {
+                indexDia = i;
+                ingresoMayor = ingresosDiarios[i];
+            }
+
+        return intToDay(indexDia);
+    }
+
 }
